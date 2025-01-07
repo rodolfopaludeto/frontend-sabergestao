e7837aaf5df18af1afd7eef7db298b5bc5618294
rodolfopaludeto <rodolfopaludeto@gmail.com>
2025-01-07 07:44:47 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import App from './App'; // Certifique-se de que o App.jsx está sendo exportado corretamente
+import './index.css'; // Inclua estilos globais aqui, se necessário
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
